b993193dc013433c989f2204c42a9059dd47948c
Skadi <fant1maz@yandex.ru>
2022-07-08 19:20:50 +0300

    example name

---
+++ Examples_005_name/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your name ");
+string username =  Console.ReadLine();
+
+if (username.ToLower() == "masha"){
+    Console.WriteLine("Yeah, this is Masha!"); 
+} else {
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
